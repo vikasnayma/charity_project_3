8ad7101e1a4e3b792bdf426495601e20c8ec108c
Singh8871Shivani <singh8871shivani@gmail.com>
2024-11-09 19:32:43 +0530

    Login/Signup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,9 +6,10 @@ import Home from './Pages/Home'
 import Projects from './Pages/Projects'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
-import Dashboard from './Pages/Dashboard';
+import Dashboard from './Pages/DonorProfile';
 import Login from './Pages/Login'
-import Volunteer from './Pages/Volunteer';
+import Volunteer from './Pages/VolunteerProfile';
+import Signup from './Pages/Signup';
 
 function App() {
   return (
@@ -23,8 +24,9 @@ function App() {
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Login" element={<Login />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Donor" element={<Dashboard />} />
         <Route path="/Volunteer" element={<Volunteer/>} />
+        <Route path="/Signup" element={<Signup />} />
         </Routes>
 
        
