6cdc2ac4d4680aa42db2ff20b122f3e7642b534f
vikasnayma <vikasnaymaofficial@gmail.com>
2024-11-11 10:58:29 +0530

    app jsx updted

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,17 +7,14 @@ import Projects from './Pages/Projects'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
 import Dashboard from './Pages/Dashboard';
-<<<<<<< HEAD
 import Login from './components/login'
 import Volunteerdashboard from './Pages/Volunteerdashboard';
 import Managerdashboard from './admin-dashboard/Managerdashboard';
 import  Signup from './components/Signup';
-=======
 // import Login from './components/login'
 import Volunteer from './Pages/Volunteer';
 // import Signup from './Pages/Signup';
 import PaymentForm from './Pages/PaymentForm'
->>>>>>> b31205a8c528404a5078f78c56dbec9d0f6b5690
 
 function App() {
   return (
@@ -31,13 +28,11 @@ function App() {
         <Route path="/Contact" element={<Contact />} />
         {/* <Route path="/Login" element={<Login />} /> */}
         <Route path="/Donor" element={<Dashboard />} />
-<<<<<<< HEAD
         <Route path="/signup" element={<Signup />} />
         {/* <Route path="/Donordashboard" element={<Donordashboard/>} /> */}
         <Route path="/Volunteerdashboard" element={<Volunteerdashboard/>} />
         <Route path="/Managerdashboard" element={<Managerdashboard/>} />
         </Routes>
-=======
         <Route path="/Volunteer" element={<Volunteer/>} />
         {/* <Route path="/Signup" element={<Signup />} /> */}
         <Route path='/PaymentForm' element={<PaymentForm/>} />
@@ -46,7 +41,6 @@ function App() {
 
        
 
->>>>>>> b31205a8c528404a5078f78c56dbec9d0f6b5690
     </div>
   );
 }
